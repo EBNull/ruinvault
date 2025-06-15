7c5d047e709943ac2a7c68ecb7afc57b5ee5e0e2
EBNull <elise.null@gmail.com>
2025-06-15 15:50:04 -0400

    Add PatchForceHighUnityQuality and PatchForceWindowedResolution1440p

--- ruinvault/patch-visuals.cs
+++ ruinvault/patch-visuals.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Microsoft.Win32;
 
 namespace ruinvault;
 // FrameSwitcher.DropGhost
@@ -16,5 +17,30 @@ internal class PatchNoGhosts
     {
         Tools.MaybeLogInfo(100, "skipping ghost drop");
         return false;
+    }
+}
+
+// These quality and resolution hacks apply too late for the current session, but they apply
+// next time the game opens.
+
+[Feature(DefaultEnabled = true)]
+internal class PatchForceHighUnityQuality
+{
+    public PatchForceHighUnityQuality()
+    {
+        var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Inkle Ltd\\Heaven's Vault", true);
+        key.SetValue("UnityGraphicsQuality_h1669003810", 100, RegistryValueKind.DWord);
+    }
+}
+
+[Feature(DefaultEnabled = false)]
+internal class PatchForceWindowedResolution1440p
+{
+    public PatchForceWindowedResolution1440p()
+    {
+        var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Inkle Ltd\\Heaven's Vault", true);
+        key.SetValue("Screenmanager Is Fullscreen mode_h3981298716", 0, RegistryValueKind.DWord);
+        key.SetValue("Screenmanager Resolution Height_h2627697771", 1440, RegistryValueKind.DWord);
+        key.SetValue("Screenmanager Resolution Width_h182942802", 2560, RegistryValueKind.DWord);
     }
 }
