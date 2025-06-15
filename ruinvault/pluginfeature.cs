9159e698b0415ebe4240c70b1966de0fae4a236d
EBNull <elise.null@gmail.com>
2025-06-15 16:20:40 -0400

    Correct feature initialization to correctly engage with Unity's object lifecycle

--- ruinvault/pluginfeature.cs
+++ ruinvault/pluginfeature.cs
@@ -1,12 +1,16 @@
 
 using System;
+using System.CodeDom;
 using System.Collections;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Xml.Serialization;
 using HarmonyLib;
 using ruinvault;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class FeatureData(Type type, string Name = "", string Description = "", bool DefaultEnabled = false, bool SurviveUnload = false, int Priority = 0)
 {
@@ -77,8 +81,17 @@ public class WrapFeature(Type feature, string harmonyBaseId, GameObject? unityPa
         return feature.Name;
     }
 
+    private static bool HasMethod(Type typ, string methodName)
+    {
+        return typ.GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly) != null;
+    }
+
     virtual public bool Enable()
     {
+        // TODO: Perhaps construct first, and then call an init method
+
+        // Harmony patch lifecycle
+
         try
         {
             harmony.PatchAll(feature);
@@ -89,6 +102,47 @@ public class WrapFeature(Type feature, string harmonyBaseId, GameObject? unityPa
             return false;
         }
         Tools.LogInfo($"Applied patches for {Name()}");
+
+        // C# & Unity instance lifecycle
+
+        // Unity *really wants to be in charge* of object constructors,
+        // so we need to use .AddComponent because there's no public way
+        // to use an existing component and attach it to a gameobject.
+
+        // First, we try it Unity's way :/
+
+        // Unity game loop lifecycle
+        var b = MaybeFeatureAsUnityComponent(feature, unityParent);
+
+        if (b is not null)
+        {
+            // Did it, do some unity stuff and assign the instance
+
+            // Unity game loop lifecycle - attach parent and set active
+            if (unityParent is not null)
+            {
+                Tools.LogError($"Setting {b} as child of {unityParent}");
+                b.transform.parent = unityParent.transform;
+            }
+            b.gameObject.SetActive(true);
+
+            var mb = b as MonoBehaviour;
+            if (mb is not null)
+            {
+                mb.enabled = true;
+
+                if (!mb.isActiveAndEnabled)
+                {
+                    Tools.MessageBox("!b.isActiveAndEnabled", $"feature {feature.Name} is not activeAndEnabled");
+                }
+
+            }
+
+            instance = b;
+            return true;
+        }
+
+        // This wasn't part of Unity, activate it ourselves
         try
         {
             instance = Activator.CreateInstance(feature);
@@ -97,14 +151,84 @@ public class WrapFeature(Type feature, string harmonyBaseId, GameObject? unityPa
         {
             Tools.LogError($"Could not instantiate {Name()}: {e}");
             Disable();
+            return false;
         }
-        var go = instance as MonoBehaviour;
-        if (unityParent != null && go != null)
+        if (instance is null)
         {
-            Tools.LogError($"Setting {go} as child of {unityParent}");
-            go.transform.parent = unityParent.transform;
+            Tools.LogError($"Could not instantiate {Name()}: Activator.CreateInstance returned null");
+            Disable();
+            return false;
         }
+
         return true;
+    }
+
+
+    static readonly string[] allUnityMethods = ["Awake", "Start", "Update", "FixedUpdate", "LateUpdate", "OnGUI", "OnEnable"];
+
+    Component? MaybeFeatureAsUnityComponent(Type feature, GameObject? parent)
+    {
+
+
+        var isUnitySubclass = feature.IsSubclassOf(typeof(MonoBehaviour));
+
+        string[] featUnityMethods = [.. allUnityMethods.Where(method => HasMethod(feature, method))];
+        var hasUnityMethods = featUnityMethods.Length != 0;
+
+        var shouldBeTreatedAsUnity = isUnitySubclass || hasUnityMethods;
+
+        if (shouldBeTreatedAsUnity && parent is null)
+        {
+            Tools.LogError("Could not initialize: missing parent gameObject!");
+            return null;
+        }
+
+
+        Component? co = null;
+
+        if (shouldBeTreatedAsUnity)
+        {
+            if (parent is not null)
+            {
+                co = parent.GetComponent(feature);
+                co ??= parent.AddComponent(feature);
+            }
+        }
+        var isTreatableAsUnity = co is not null;
+
+        //as MonoBehavior: {go} (== null: {go == null} ; != null: {go != null} ; is null: {go is null})
+
+        var detail = $"""
+        type: {feature}
+        
+        component: {co}
+
+        isUnitySubclass: {isUnitySubclass}
+        hasUnityMethods: {hasUnityMethods} ({featUnityMethods.Join()})
+                             
+        shouldBeTreatedAsUnity: {shouldBeTreatedAsUnity}
+        isTreatableAsUnity: {isTreatableAsUnity}
+        """;
+
+        var errTitle = "";
+        var errMsg = "";
+        if (!isTreatableAsUnity && shouldBeTreatedAsUnity)
+        {
+            errTitle = "Bad Feature";
+            errMsg = $"Feature {feature.Name} seems to want to be in unison, but is !isUnityAssignable:\n\n{detail}";
+        }
+        if (isTreatableAsUnity && !shouldBeTreatedAsUnity)
+        {
+            errTitle = "Unoptimal Feature";
+            errMsg = $"Feature {feature.Name} doesn't need Unity behavior, but isUnityAssignable:\n\n{detail}";
+        }
+        if (errMsg != "")
+        {
+            Tools.LogError(errMsg);
+            Tools.MessageBox(errTitle, errMsg);
+        }
+
+        return co;
     }
 
     virtual public void Disable()
